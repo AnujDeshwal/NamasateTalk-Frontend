922880748db10093548f26a012271fd55f31bd04
AnujDeshwal <anujdeshwal95@gmail.com>
2024-06-21 09:15:51 +0530

    Backend deployed on render

--- src/constants/server.js
+++ src/constants/server.js
@@ -1,2 +1,3 @@
-export const server = 'https://namaste-talk-backend.vercel.app'
+// export const server = 'https://namaste-talk-backend.vercel.app'
+export const server = 'https://namastetalk-backend.onrender.com'
 // export const server = 'http://localhost:8000'
